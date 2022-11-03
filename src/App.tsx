b13413dc55a487ca80b7e91522016032674db9cb
HenrikVL <henrik.viken.lied@gmail.com>
2022-11-03 15:59:36 +0100

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { HeaderResponsive } from "./components/Header";
+import { AppShell, MantineProvider } from "@mantine/core";
+
+const links = {
+  links: [
+    {
+      link: "/about",
+      label: "🙋‍♂️ About me",
+    },
+    {
+      link: "/projects",
+      label: "📈 Projects",
+    },
+    {
+      link: "/interests",
+      label: "🧗‍♂️ Interests",
+    },
+    {
+      link: "/blog",
+      label: "✍️ Blog",
+    },
+  ],
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Henrikvl</h1>
+      <MantineProvider
+        theme={{ colorScheme: "light" }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <AppShell
+          fixed={true}
+          header={<HeaderResponsive links={links.links} />}
+        >
+          content
+        </AppShell>
+      </MantineProvider>
     </div>
   );
 }
