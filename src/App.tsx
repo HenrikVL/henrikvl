bd63ab5138ef1b03c255a671b081365b82e150c9
HenrikVL <henrik.viken.lied@gmail.com>
2022-11-03 13:43:11 +0100

    feat: convert to react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Henrikvl</h1>
+    </div>
+  );
+}
+
+export default App;
